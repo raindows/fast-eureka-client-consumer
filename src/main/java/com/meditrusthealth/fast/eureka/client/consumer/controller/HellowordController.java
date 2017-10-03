4b6de93a07c71c9422dcac0750bcb9cbb52ac598
rainy.wang <582295447@qq.com>
2017-10-03 19:40:36 +0800

    update

--- src/main/java/com/meditrusthealth/fast/eureka/client/consumer/controller/HellowordController.java
+++ src/main/java/com/meditrusthealth/fast/eureka/client/consumer/controller/HellowordController.java
@@ -3,7 +3,10 @@
  */
 package com.meditrusthealth.fast.eureka.client.consumer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,18 +21,30 @@ import org.springframework.web.client.RestTemplate;
  * @version
  */
 @RestController
-@RequestMapping(name="/consumer")
+@RequestMapping(value = "/consumer")
 public class HellowordController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(HellowordController.class);
+
 	private static final String URL = "http://fast-eureka-client-provider/provider/hello";
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@GetMapping(name = "/hello")
+	@Value("${app.env}")
+	private String env;
+
+	@GetMapping(value = "/hello")
 	public String hello() {
 		String message = restTemplate.getForEntity(URL, String.class).getBody();
 		// return "Consumer Helloword";
+		LOG.info("message:{}", message);
 		return message;
+	}
+
+	@GetMapping(value = "/config")
+	public String config() {
+		LOG.info("app.env:{}", env);
+		return env;
 	}
 }
