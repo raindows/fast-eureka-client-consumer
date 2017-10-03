644e9de9757e4fffe7d40ca097c487685d51d7aa
rainy.wang <582295447@qq.com>
2017-10-03 15:23:06 +0800

    init

---
+++ src/main/java/com/meditrusthealth/fast/eureka/client/consumer/FastEurekaClientConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.meditrusthealth.fast.eureka.client.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastEurekaClientConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastEurekaClientConsumerApplication.class, args);
+	}
+}
